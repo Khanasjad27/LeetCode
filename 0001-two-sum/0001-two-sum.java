f0ef5ee2fd788f7ab9da4563508d10c8692d61f0
Asjad Khan <khanasjadshamshadahmed@gmail.com>
2026-07-23 19:45:34 +0530

    Time: 2 ms (99.3%), Space: 47.1 MB (58.63%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,32 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+
+        // HashMap banaya hai jisme
+        // Key = Array ka element
+        // Value = Us element ka index
         HashMap<Integer, Integer> numToIdx = new HashMap<>();
 
-        for(int i=0; i<nums.length; i++){
-            int remaingValue = target - nums[i];
+        // Pure array ko ek baar traverse karenge
+        for (int i = 0; i < nums.length; i++) {
 
-            if(numToIdx.containsKey(remaingValue)){
-                return new int[] { numToIdx.get(remaingValue),i };
+            // Current element ke liye required complement nikala
+            // jisse add karke target ban sake
+            int remainingValue = target - nums[i];
+
+            // Agar complement pehle se HashMap me present hai
+            // matlab pair mil gaya
+            if (numToIdx.containsKey(remainingValue)) {
+
+                // Complement ka index aur current index return kar do
+                return new int[] { numToIdx.get(remainingValue), i };
             }
 
+            // Agar pair nahi mila
+            // to current element aur uska index HashMap me store kar do
             numToIdx.put(nums[i], i);
         }
+
+        // Agar koi pair nahi mila to empty array return kar do
         return new int[] {};
     }
 }
