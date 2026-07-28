3a85820724a1db466443306559082dc75bb37b70
Asjad Khan <khanasjadshamshadahmed@gmail.com>
2026-07-28 13:44:53 +0530

    Time: 4 ms (88.57%), Space: 45.5 MB (74.01%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,21 +1,42 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int i = 0;
-        int j = 0;
-        int maxLength = Integer.MIN_VALUE;
-        HashSet<Character> set = new HashSet<>();
+
+        // Using HashSet => T.C (O(2N))
+
+        // int i = 0;
+        // int j = 0;
+        // int maxLength = Integer.MIN_VALUE;
+        // HashSet<Character> set = new HashSet<>();
+        // while(j<s.length()){
+            
+        //     if(set.contains(ch)){
+        //         while(i<j && set.contains(ch)){
+        //             set.remove(s.charAt(i));
+        //             i++;
+        //         }
+        //     }
+        //     set.add(ch);
+        //     maxLength = Math.max(maxLength, j-i+1);
+        //     j++;
+        // }    
+        // return (maxLength == Integer.MIN_VALUE )? 0 : maxLength;
+
+        // Using HashMap => T.C (O(N))
+
+        int i = 0, j = 0;
+        int maxL = Integer.MIN_VALUE;
+        HashMap<Character, Integer> map = new HashMap<>();
+        
         while(j<s.length()){
-            char ch = s.charAt(j); 
-            if(set.contains(ch)){
-                while(i<j && set.contains(ch)){
-                    set.remove(s.charAt(i));
-                    i++;
-                }
+            char ch = s.charAt(j);
+            
+            if (map.containsKey(ch)) {
+                i = Math.max(i, map.get(ch) + 1); // Prevents 'i' from jumping backward
             }
-            set.add(ch);
-            maxLength = Math.max(maxLength, j-i+1);
+            map.put(ch, j);
+            maxL = Math.max(maxL, j-i+1);
             j++;
-        }    
-        return (maxLength == Integer.MIN_VALUE )? 0 : maxLength;
+        }
+        return (maxL == Integer.MIN_VALUE)? 0: maxL;
     }
 }
